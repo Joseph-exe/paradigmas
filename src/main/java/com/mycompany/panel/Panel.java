6cb9d7d90f5f5187a611c707c4827857792ad62e
Alumno <Alumno@10.40.5.134>
2023-05-02 10:56:17 -0400

    Primera clase con Git

---
+++ src/main/java/com/mycompany/panel/Panel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.panel;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Panel 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
